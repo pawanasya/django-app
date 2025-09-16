6e2653ffc566871d8ea859b4d52300e4338729c5
Pawan Khandelwal <pawan@alphamoney.in>
2025-09-16 17:19:49 +0530

    changes23

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
-# Wait for MySQL to be ready
 echo "Waiting for MySQL to be ready..."
-until mysqladmin ping -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent --ssl-mode=DISABLED; do
+
+# Use Python + PyMySQL to check connection instead of mysqladmin
+until python -c "
+import pymysql, os, sys
+try:
+    conn = pymysql.connect(
+        host=os.environ.get('DB_HOST'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        database=os.environ.get('DB_NAME'),
+        port=int(os.environ.get('DB_PORT', 3306)),
+        ssl={'ssl':{}}
+    )
+    conn.close()
+except Exception as e:
+    sys.exit(1)
+" 2>/dev/null; do
     echo "Waiting for MySQL..."
     sleep 5
 done
 
-# Run migrations
+echo "MySQL is ready. Running migrations..."
 python manage.py migrate
 
-# Start Django server
+echo "Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
